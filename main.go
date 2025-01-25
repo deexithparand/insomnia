69ba10a4d16969dfd4d0f44721c86bd4f11c9f37
Deexith Parandaman <deexith@google.local>
2025-01-25 20:27:13 +0530

    feat : restart

--- main.go
+++ main.go
@@ -5,10 +5,10 @@ import (
 	"insomnia/config"
 	"insomnia/database"
 	"insomnia/state"
-	"log"
 )
 
 func main() {
+
 	// load the config from env
 	var (
 		cfg      config.Config
@@ -20,15 +20,16 @@ func main() {
 
 	// load config from env
 	err := cfg.LoadConfig(env)
+
 	if err != nil {
-		log.Fatalf("Error loading config: %v", err)
+		panic(err)
 	}
 
 	// configure db connections
 	database.ConfigureDSN(cfg)
 	err = database.Connect()
 	if err != nil {
-		log.Fatalf("error connecting to database : %v", err)
+		panic(err)
 	}
 
 	// migrate db
